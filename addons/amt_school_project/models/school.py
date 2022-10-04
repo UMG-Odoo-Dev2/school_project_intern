e60d03ffa2edf245cb820eddc181fbbf11129b70
unknown <aungmyat.thu.1388@gmail.com>
2022-10-04 08:01:03 +0630

    3_Oct_School_Proj Update

--- addons/amt_school_project/models/school.py
+++ addons/amt_school_project/models/school.py
@@ -8,17 +8,21 @@ class SchoolProject(models.Model):
     name = fields.Char(string="Full Name", required=True)
     photo = fields.Binary(string="Profile Picture")
     roll_no = fields.Char()
-    role = fields.Selection([('student','Student'),('teacher','Teacher')], label="Role", required=True)
-    #state = fields.Selection([('old','Old'),('new','New'),('current','Current')], label="State", required=True)
+    role = fields.Selection([('student','Student'),('teacher','Teacher')], string="Role", required=True)
+    state = fields.Selection([
+        ('old','Old'),
+        ('new','New'),
+        ('current','Current'),
+    ], string="State", copy=False, tracking=True, default="current")
     father_name = fields.Char(string="Father Name")
     dob = fields.Date(string="Date of Birth")
     address = fields.Text(string="Address")
-    phone_no = fields.Integer(string="Phone Number")
+    phone_no = fields.Char(string="Phone Number")
 
 
-    state = fields.Selection(selection=[
-       ('old', 'Old'),
-       ('new', 'New'),
-       ('current', 'Current'),
-   ], string='State', required=True, read=True, write=True, copy=False,
-   tracking=True)
+#     status = fields.Selection(selection=[
+#        ('old', 'Old'),
+#        ('new', 'New'),
+#        ('current', 'Current'),
+#     ], string='Status', required=True, read=True, write=True, copy=False,
+#    tracking=True)
