df7090f8adccd91ace6c2cf3dc97e649a657ceba
unknown <wintwarko.slm4@gmail.com>
2022-10-04 07:31:04 +0630

    3_10_22 School Project Update

--- addons/student_wwk/models/school_project.py
+++ addons/student_wwk/models/school_project.py
@@ -6,17 +6,22 @@ class SchoolProject(models.Model):
 
     name = fields.Char('Full Name:')
     roll_num = fields.Char('Roll Number:')
+    gender = fields.Selection([('male','Male'),('female','Female')],'Gender:')
     role = fields.Selection([('student','Student'),('teacher','Teacher')],'Role:')
     state = fields.Selection([('presence','Presence'),('absence','Absence'),('former','Former')],'State:')
     dob = fields.Date('Date of Birth:')
     father_name = fields.Char('Father Name:')
     mother_name = fields.Char('Mother Name:')
+    kanban_state = fields.Selection([('normal', 'Grey'),('done', 'Green'),('blocked', 'Red')], string='Kanban State', copy=False, default='normal', required=True)
     ph_no = fields.Char('Phone Number:')
     email = fields.Char('Email:')
+    avator = fields.Binary()
     status = fields.Selection(selection=[
        ('presence', 'Presence'),
        ('absence', 'Absence'),
        ('former', 'Former'),
    ], string='Status', required=True,readonly=True,  copy=False,
    tracking=True)
+   
+   
 
